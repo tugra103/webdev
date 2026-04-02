40386ccc42fddd164e6c5c29f37abd76f0d45f33
tugra103 <tugrabulbul2020@gmail.com>
2026-04-02 17:27:25 +0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,6 @@
 import { FingerprintProvider } from '@fingerprint/react'
 import {AppProps} from 'next/app'
 
-export default function MyApp({Component, pageProps}: AppProps) {
-  return (
-    <FingerprintProvider
-      apiKey="GZPxUkuA0MLMwf37sk9s"
-      region="eu"
-    >
-      <Component {...pageProps} />
-    </FingerprintProvider>
-  )
-}
 import {useVisitorData} from '@fingerprint/react'
 
 export default function Home() {
