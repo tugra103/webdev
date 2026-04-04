3343865d96219c6bfa3bef69e0285d5e07bf6278
tugra103 <tugrabulbul2020@gmail.com>
2026-04-04 08:56:28 +0300

    .

--- app/main/account/page.tsx
+++ app/main/account/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { useAuth } from "../../context/AuthContext";
+import { useAuth } from "../../../context/AuthContext";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import Navbar from "../../comporents/navbar";
+import Navbar from "../../../comporents/navbar";
 import { Card} from 'primereact/card'
 
 export default function Page() {
