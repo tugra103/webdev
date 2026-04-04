b7fc278acd00fe32f87825a1b869588911d42142
tugra103 <tugrabulbul2020@gmail.com>
2026-04-04 08:40:41 +0300

    .

--- app/main/games/page.tsx
+++ app/main/games/page.tsx
@@ -3,16 +3,22 @@
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import Navbar from "@comporents/navbar";
+import Navbar from "../../../comporents/navbar";
 
 export default function Page() {
   const { user } = useAuth();
-  if (!user) return null;
-
-  return (
+  const {router} = useRouter()
+  if (user) {
+    return (
     <div>
       <Navbar />
       <div>Sayfa içeriği burada</div>
     </div>
-  );
+    );
+  } else {
+    router.push("/login/sign-in")
+    return (<div>
+      Loading...
+    </div>)
+  }
 }
--- app/main/page.tsx
+++ app/main/page.tsx
@@ -3,7 +3,7 @@
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import Navbar from "@comporents/navbar";
+import Navbar from "../../comporents/navbar";
 
 export default function Page() {
   const { user } = useAuth();
