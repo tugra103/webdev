14e9734b540e66d3f11299dab75abc792303af7d
tugra103 <tugrabulbul2020@gmail.com>
2026-04-06 10:34:59 +0300

    .

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -3,7 +3,15 @@
 import { auth } from "@/app/firebase"; // ✅ artık import ediliyor
 import { createContext, useContext, useEffect, useState } from "react";
 import { onAuthStateChanged, User } from "firebase/auth";
+type AuthContextType = {
+  user: User | null;
+  loading: boolean;
+};
 
+const AuthContext = createContext<AuthContextType>({
+  user: null,
+  loading: true,
+});
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
