a48d6517a3afd58af0b2d723abfe60fa89add60e
tugra103 <tugrabulbul2020@gmail.com>
2026-04-06 19:56:18 +0300

    .

--- app/main/games/page.tsx
+++ app/main/games/page.tsx
@@ -15,10 +15,21 @@ export default function Page() {
   if (loading) return <ProgressSpinner /> // veya <Spinner />  ← BU OLMAZSA refresh'te user=null görür ve redirect eder
 
   if (!user) router.push("/webdev/login/sign-in");
+    const itemRenderer = (item: any) => (
+      <a className="flex align-items-center p-menuitem-link" style={{ gap: "10px" }} href={item.url ?? ""}>
+        <span className={item.icon} />
+        <span style={{ fontFamily: "Syne, sans-serif" }}>{item.label}</span>
+        {item.badge && <Badge value={item.badge} />}
+        {item.shortcut && <span className="shortcut-pill">{item.shortcut}</span>}
+      </a>
+    );
+  
     return (
     <div className="theme1">
       <Navbar />
-      <div>Sayfa içeriği burada</div>
+      <div>
+
+      </div>
     </div>
     );
 }
