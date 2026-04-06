87f6faea94f7915f0ec728d7fcb7fc69aeed9686
tugra103 <tugrabulbul2020@gmail.com>
2026-04-06 09:31:52 +0300

    .

--- app/firebase.tsx
+++ app/firebase.tsx
@@ -1,7 +1,7 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, browserLocalPersistence, setPersistence  } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
