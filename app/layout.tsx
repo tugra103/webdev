d79886b71e89aba1d83efb9da20a43b622aa372e
tugra103 <tugrabulbul2020@gmail.com>
2026-04-07 17:44:50 +0300

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { FingerPrinter } from "@/context/fingerprint";
+import { FingerprintProvider } from '@fingerprint/react'
 import { AuthProvider } from "@/context/AuthContext";
 
 
@@ -33,9 +33,12 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">        
         <AuthProvider>
-          <FingerPrinter>
-            {children}
-          </FingerPrinter>
+        <FingerprintProvider
+          apiKey="GZPxUkuA0MLMwf37sk9s"
+          region="eu"
+        >
+          {children}
+        </FingerprintProvider>
         </AuthProvider>
         </body>
     </html>
