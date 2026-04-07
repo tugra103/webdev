93b96473c60afeb8aecf4a583f8fd8561dfbac5f
tugra103 <tugrabulbul2020@gmail.com>
2026-04-07 17:21:44 +0300

    .

--- context/fingerprint.tsx
+++ context/fingerprint.tsx
@@ -5,7 +5,7 @@ import { FingerprintProvider } from '@fingerprint/react'
 export function FingerPrinter({ children }: { children: React.ReactNode }) {
   return (
     <FingerprintProvider
-          apiKey="GZPxUkuA0MLMwf37sk9s"
+          apiKey="EL5ywQASbag88DLIMvWw"
           region="eu"
         >
       {children}
