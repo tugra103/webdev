606021a271c9a3ff165f62d5a1e25a302027d008
tugra103 <tugrabulbul2020@gmail.com>
2026-04-22 15:58:45 +0300

    .

--- comporents/message.tsx
+++ comporents/message.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 
-const Message = ({chat, user}) => (
+interface Chat {
+    username: string;
+    content: ReactElement;
+    img: string;
+}
+
+interface MessageProps {
+    chat: Chat;
+    user: string;
+}
+
+const Message = ({ chat, user }: MessageProps) => (
     <li className={`chat ${user === chat.username ? "right" : "left"}`}>
         {user !== chat.username
             && <img src={chat.img} alt={`${chat.username}'s profile pic`} />
