50fbedcc15f3c691a693fec05ed8b6eadd4cda62
tugra103 <tugrabulbul2020@gmail.com>
2026-04-22 14:48:32 +0300

    .

--- app/main/ai/page.tsx
+++ app/main/ai/page.tsx
@@ -48,7 +48,7 @@ export default function Page() {
 
                 {/* Logo / Title */}
                 <div className="text-5xl font-normal tracking-tight text-gray-700 select-none">
-                    <img src="/webdev/public/donutexe.png" />
+                    <img src="/webdev/donutexe.png" />
                 </div>
 
                 {/* Search bar */}
